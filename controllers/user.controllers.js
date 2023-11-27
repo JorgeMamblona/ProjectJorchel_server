c8cda019134480c8d9395eadb9f76cb39b9a0b98
Jormag <jorgemamblonagomez@gmail.com>
2023-11-27 14:49:53 +0100

    fix

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -6,7 +6,7 @@ const listAllUsersHandler = (req, res, next) => {
     User
         .find()
         .sort({ username: 1 })
-        .select({ username: 1 })
+        .select({ username: 1, image: 1 })
         .then(response => res.status(200).json(response))
         .catch(err => next(err))
 
